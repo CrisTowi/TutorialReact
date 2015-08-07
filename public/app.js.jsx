d6dd7d58b5ee5a622893d4bb9faebfd606530087
Christian Consuelo <cconsuelo@intelimetrica.com>
2015-08-07 10:29:50 -0500

    Agregar funcionalidad con botón

--- public/app.js.jsx
+++ public/app.js.jsx
@@ -1,18 +1,53 @@
 /** @jsx React.DOM */
 
 $(document).ready(function(){
-	DivComponent = React.createClass({
+
+	GeneralComponent = React.createClass({
+		getInitialState: function() {
+			return ({
+				mensaje: 'Hola mundo'
+			});
+		},
+		handleClick: function() {
+			this.setState({
+				mensaje: 'Adios mundo'
+			});
+		},
 		render: function() {
 			return (
 				<div>
-					<h1>Hola mundo</h1>
+					<DivComponent 
+						texto={this.state.mensaje} />
+					<ButtonComponent 
+						handleClick={this.handleClick} />
 				</div>
 			);
 		}
 	});
 
+	DivComponent = React.createClass({
+		render: function() {
+			return (
+				<div>
+					<h1>{this.props.texto}</h1>
+				</div>
+			);
+		}
+	});
+
+	ButtonComponent = React.createClass({
+		handleClick: function() {
+			this.props.handleClick();
+		},
+		render: function() {
+			return (
+				<button onClick={this.handleClick}>Cambiar mensaje</button>
+			);
+		}
+	});
+
 	React.render(
-		<DivComponent />,
+		<GeneralComponent />,
 		document.getElementById('container')
 	);
 });
